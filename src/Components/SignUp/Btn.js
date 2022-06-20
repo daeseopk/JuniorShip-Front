8f20d3a0af5b2b683d87fa13a40cdae32991db1d
DaeSeop <eotjqdlee@gmail.com>
2022-06-20 23:01:51 +0900

    2022/06/20

--- src/Components/SignUp/Btn.js
+++ src/Components/SignUp/Btn.js
@@ -1,33 +1,72 @@
 import React from "react";
 import styles from "./Btn.module.css";
+import styled from "styled-components";
 import { setStatus_plus, setStatus_minus } from "../../Redux/status";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
-export default function Btn({ select }) {
+const Button = styled.div`
+   display: flex;
+   width: 120px;
+   height: 50px;
+   background-color: ${(prop) => prop.backgroundcolor};
+   border-radius: 10px;
+   margin-right: 20px;
+   margin-left: 20px;
+   align-items: center;
+   justify-content: center;
+   visibility: ${(prop) => prop.visibility};
+   opacity: ${(prop) => prop.opacity};
+   transform: ${(prop) => prop.transform};
+   transition: 0.3s ease all;
+`;
+
+export default function Btn({ select, activeBtn, setActiveBtn }) {
    const status_redux = useSelector((state) => state.status.value.status);
-
+   const a = false; // 2페이지 버튼 활성화 조건(테스트)
    const dispatch = useDispatch();
    const onClickBtn = (e) => {
-      if (select) {
-         // 주니어, 아마추어, 시니어가 선택됐을 때만 동작
-         if (e.target.id === "prev") {
-            if (status_redux === 0) return;
-            else dispatch(setStatus_minus());
-         } else if (e.target.id === "next") {
-            if (status_redux === 4) return;
-            else dispatch(setStatus_plus());
-         }
-      } else alert("단계를 먼저 선택해 주세요. (임시 팝업)");
+      // 버튼 활성화 조건이 충족될 떄만 동작
+      if (e.target.id === "prev") {
+         if (status_redux === 0) return;
+         else dispatch(setStatus_minus());
+      } else if (e.target.id === "next") {
+         if (status_redux === 4) return;
+         else dispatch(setStatus_plus());
+      }
    };
+   useEffect(() => {
+      var activeBtn_ = activeBtn;
+      // if(status_redux===페이지 && (페이지별 버튼 활성화 조건)){
+      //    activeBtn_=true;
+      // }
+      if (status_redux === 0 && select) {
+         activeBtn_ = true;
+      } else if (status_redux === 1 && a) {
+         activeBtn_ = true;
+      } else activeBtn_ = false;
+
+      setActiveBtn(activeBtn_);
+   }, [status_redux, select]); // 페이지와 페이지별 버튼 활성화 조건이 업데이트 될 떄마다 실행
+
    return (
       <div className={styles.BtnContainer}>
-         <div id="prev" onClick={onClickBtn} className={styles.Btn}>
+         <Button
+            backgroundcolor="skyblue"
+            visibility={status_redux === 0 ? "hidden" : "visible"}
+            opacity={status_redux === 0 ? "0" : "1"}
+            id="prev"
+            onClick={onClickBtn}>
             이전
-         </div>
-         <div id="next" onClick={onClickBtn} className={styles.Btn}>
+         </Button>
+         <Button
+            transform={status_redux === 0 ? "translate(-65%, 0)" : "none"}
+            id="next"
+            backgroundcolor={activeBtn ? "skyblue" : "gray"} // 버튼 활성화 조건에 따라 버튼 비활성화
+            onClick={activeBtn ? onClickBtn : null}>
             다음
-         </div>
+         </Button>
       </div>
    );
 }
--- src/Routes/SignUp/SignUp.js
+++ src/Routes/SignUp/SignUp.js
@@ -5,6 +5,7 @@ import SignUpOne from "../../Components/SignUp/SignUpOne";
 import Btn from "../../Components/SignUp/Btn";
 
 export default function SignUp() {
+   const [activeBtn, setActiveBtn] = useState(false); // 버튼 활성화 조건
    const [select, setSelect] = useState(); // 주니어 아마추어 시니어
 
    return (
@@ -12,7 +13,11 @@ export default function SignUp() {
          <div className={styles.SignUpContainer}>
             <ProgressBar />
             <SignUpOne setSelect={setSelect} />
-            <Btn select={select} />
+            <Btn
+               select={select}
+               activeBtn={activeBtn}
+               setActiveBtn={setActiveBtn}
+            />
          </div>
       </div>
    );
